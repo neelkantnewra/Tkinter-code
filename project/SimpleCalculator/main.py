c845ed393afc408227a87d6dc7947d28cdf80fa6
Neelkant Newra <newra008@gmail.com>
2021-03-16 11:00:01 +0530

    Update main.py

--- project/SimpleCalculator/main.py
+++ project/SimpleCalculator/main.py
@@ -70,8 +70,6 @@ button_1.grid(row=3,column=0)
 button_2.grid(row=3,column=1)
 button_3.grid(row=3,column=2)
 button_mul.grid(row=3,column=3)
-button_add.grid(row=4,column=2,rowspan=2)
-button_min.grid(row=4,column=3)
 
 button_4.grid(row=2,column=0)
 button_5.grid(row=2,column=1)
@@ -85,8 +83,10 @@ button_back.grid(row=1,column=3)
 
 button_dot.grid(row=4,column=0)
 button_0.grid(row=4,column=1)
-button_equal.grid(row=5,column=3)
+button_add.grid(row=4,column=2,rowspan=2)
+button_min.grid(row=4,column=3)
 
+button_equal.grid(row=5,column=3)
 button_clear.grid(row=5,column=0,columnspan=2)
 
 root.mainloop()
